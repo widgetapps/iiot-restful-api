46a6cd74a9f5be8d062b930b028fb8c14db6ca68
Darryl Patterson <widgetapps@gmail.com>
2017-05-31 12:55:46 -0400

    Working on endpoints...

--- app/controllers/assets.server.controller.js
+++ app/controllers/assets.server.controller.js
@@ -127,7 +127,7 @@ exports.addDevice = function(req, res) {
                              }
 
                              res.status(200).send({
-                                 message: 'Device ' + device.serialNumber + 'added to asset ' + asset.tagCode
+                                 message: 'Device ' + device.serialNumber + ' added to asset ' + asset.tagCode
                              });
                          });
 
