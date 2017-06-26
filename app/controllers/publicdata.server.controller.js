4049cd46da768c28db3cd874225d286bd005d34e
Darryl Patterson <widgetapps@gmail.com>
2017-06-26 00:50:11 -0400

    Fixed a date thing.

--- app/controllers/publicdata.server.controller.js
+++ app/controllers/publicdata.server.controller.js
@@ -10,26 +10,26 @@ exports.index = function(req, res) {
 
 exports.aggregatedTemperature = function(req, res) {
     Telemetry.aggregate([
-        {$match: {timestamp: {$gte: moment().subtract(1, 'days')}, "tag.sensorTagCode": "TI"}},
-        {$group: {
-            _id: {
-                year: { $year: "$timestamp" },
-                month: { $month: "$timestamp" },
-                day: { $dayOfMonth: "$timestamp" },
-                hour: { $hour: "$timestamp" },
-                "minute": {
-                    $subtract: [
-                        { $minute: "$timestamp" },
-                        { $mod: [{ $minute: "$timestamp" }, 30] }
+        {'$match': {'timestamp': {'$gte': moment().subtract(1, 'days')}, 'tag.sensorTagCode': 'TI'}},
+        {'$group': {
+            '_id': {
+                'year': { '$year': '$timestamp' },
+                'month': { '$month': '$timestamp' },
+                'day': { '$dayOfMonth': '$timestamp' },
+                'hour': { '$hour': '$timestamp' },
+                'minute': {
+                    '$subtract': [
+                        { '$minute': '$timestamp' },
+                        { '$mod': [{ '$minute': '$timestamp' }, 30] }
                     ]
                 }
             },
-            count: {$sum: 1},
-            min: {$min: "$data.values.min"},
-            max: {$max: "$data.values.max"},
-            average: {$avg: "$data.values.average"}
+            'count': {'$sum': 1},
+            'min': {'$min': '$data.values.min'},
+            'max': {'$max': '$data.values.max'},
+            'average': {'$avg': '$data.values.average'}
         }},
-        {$sort: {"_id.year": 1, "_id.month": 1, "_id.day": 1, "_id.hour": 1, "_id.minute": 1}}
+        {'$sort': {'_id.year': 1, '_id.month': 1, '_id.day': 1, '_id.hour': 1, '_id.minute': 1}}
     ], function (err, result) {
         if (err) {
             res.status(500).send({
