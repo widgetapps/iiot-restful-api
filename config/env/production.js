faeac00d40fc6cce91805ded75f47a26ca092ec3
Darryl Patterson <widgetapps@gmail.com>
2018-12-10 10:35:53 -0500

    DB connection issues.

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://10.240.162.13,10.240.253.155/one-platform',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://10.240.162.13/one-platform',
     mqttoptions: {
         clientId: 'worker_hydrant',
         username: 'worker',
