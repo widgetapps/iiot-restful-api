055f1ec0f08931fd23e82753651923ebb3b93c39
Darryl Patterson <widgetapps@gmail.com>
2018-12-13 22:57:20 -0500

    Trying RSA keys...

--- app/controllers/auth.server.controller.js
+++ app/controllers/auth.server.controller.js
@@ -41,63 +41,79 @@ exports.login = function(req, res) {
                             message: 'No client associated with user, your are an orphan!'
                         });
                     } else {
-                        var diffHell = crypto.createDiffieHellman(1536);
-                        diffHell.generateKeys();
-
-                        var privateKey = diffHell.getPrivateKey('hex');
-                        var publicKey = diffHell.getPublicKey('hex');
-
-                        user.pki.privateKey = privateKey;
-                        user.pki.publicKey = publicKey;
-
-                        User.findByIdAndUpdate(
-                            user._id,
-                            {
-                                '$set': {
-                                    'pki.privateKey': privateKey,
-                                    'pki.publicKey': publicKey,
-                                    updated: Date.now()
-                                }
+                        crypto.generateKeyPair('rsa', {
+                            modulusLength: 2048,
+                            publicKeyEncoding: {
+                                type: 'spki',
+                                format: 'pem'
                             },
-                            function (err, savedUser) {
-
-                                if (!savedUser || err) {
-                                    res.status(404).send({
-                                        message: 'Error saving keys.'
-                                    });
-                                }
-
-                                // Don't store private stuff in the jwt
-                                savedUser.password = undefined;
-                                savedUser.salt = undefined;
-                                savedUser.provider = undefined;
-                                savedUser.providerData = undefined;
-                                savedUser.additionalProviderData = undefined;
-                                savedUser.active = undefined;
-                                savedUser.resetPasswordToken = undefined;
-                                savedUser.resetPasswordExpires = undefined;
-                                savedUser.pki = undefined;
-
-                                // Add reseller info to the user
-                                if (client.reseller) {
-                                    savedUser.reseller = true;
-                                    savedUser.resellerClients = client.resellerClients;
-                                } else {
-                                    savedUser.reseller = false;
-                                }
-
-                                var token = jwt.sign(savedUser.toObject(), privateKey, {
-                                    expiresIn: '1d'
-                                });
-
-                                // return the information, including token, as JSON
-                                res.json({
-                                    message: 'Login successful.',
-                                    token: token,
-                                    publicKey: publicKey
+                            privateKeyEncoding: {
+                                type: 'pkcs8',
+                                format: 'pem',
+                                cipher: 'aes-256-cbc',
+                                passphrase: client.apikey.secret
+                            }
+                        }, function (err, publicKey, privateKey) {
+                            // Handle errors and use the generated key pair.
+                            if (err) {
+                                res.status(500).send({
+                                    message: 'Error generating keys.'
                                 });
                             }
-                        );
+
+                            user.pki.privateKey = privateKey;
+                            user.pki.publicKey = publicKey;
+
+                            User.findByIdAndUpdate(
+                                user._id,
+                                {
+                                    '$set': {
+                                        'pki.privateKey': privateKey,
+                                        'pki.publicKey': publicKey,
+                                        updated: Date.now()
+                                    }
+                                },
+                                function (err, savedUser) {
+
+                                    if (!savedUser || err) {
+                                        res.status(404).send({
+                                            message: 'Error saving keys.'
+                                        });
+                                    }
+
+                                    // Don't store private stuff in the jwt
+                                    savedUser.password = undefined;
+                                    savedUser.salt = undefined;
+                                    savedUser.provider = undefined;
+                                    savedUser.providerData = undefined;
+                                    savedUser.additionalProviderData = undefined;
+                                    savedUser.active = undefined;
+                                    savedUser.resetPasswordToken = undefined;
+                                    savedUser.resetPasswordExpires = undefined;
+                                    savedUser.pki = undefined;
+
+                                    // Add reseller info to the user
+                                    if (client.reseller) {
+                                        savedUser.reseller = true;
+                                        savedUser.resellerClients = client.resellerClients;
+                                    } else {
+                                        savedUser.reseller = false;
+                                    }
+
+                                    var token = jwt.sign(savedUser.toObject(), privateKey, {
+                                        expiresIn: '1d'
+                                    });
+
+                                    // return the information, including token, as JSON
+                                    res.json({
+                                        message: 'Login successful.',
+                                        token: token,
+                                        publicKey: publicKey
+                                    });
+                                }
+                            );
+
+                        });
                     }
                 });
             }
