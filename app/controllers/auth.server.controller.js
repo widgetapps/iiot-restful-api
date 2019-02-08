f8d4a906c2109fef64e00ae9c9d4e8360003e2d6
Darryl Patterson <widgetapps@gmail.com>
2019-02-08 17:40:40 -0500

    No meed to save the private key on auth.

--- app/controllers/auth.server.controller.js
+++ app/controllers/auth.server.controller.js
@@ -50,14 +50,10 @@ exports.login = function(req, res) {
                                 });
                             }
 
-                            user.pki.privateKey = privateKey;
-                            user.pki.publicKey = publicKey;
-
                             User.findByIdAndUpdate(
                                 user._id,
                                 {
                                     '$set': {
-                                        'pki.privateKey': privateKey,
                                         'pki.publicKey': publicKey,
                                         updated: Date.now()
                                     }
@@ -117,72 +113,5 @@ exports.login = function(req, res) {
 };
 
 exports.logout = function(req, res) {
-
-};
-
-exports.authenticate = function(req, res) {
-
-    var promise = User.findOne({ email: req.body.email }).exec();
-
-    console.log('PROMISES HAVE BEEN MADE');
-
-    promise.then(function (user) {
-
-        console.log('QUERY SUCCESSFUL');
-
-        if (!user) {
-            console.log('ERROR: NO USER');
-            res.status(404).send({
-                message: 'Authentication failed. User not found.'
-            });
-        } else {
-            console.log('USER FOUND');
-            if (!user.authenticate(req.body.password)) {
-                // console.log('ERROR: BAD PASSWORD - ' + user.hashPassword(req.body.password) + ' vs ' + user.password);
-                res.status(404).send({
-                    message: 'Authentication failed. Incorrect password.'
-                });
-            } else {
-                console.log('USER ID: ' + user._id);
-                Client.findById(user.client, function(err, client) {
-                    if (err) throw err;
-
-                    if (!client) {
-                        console.log('ERROR: NO CLIENT');
-                        res.status(409).send({
-                            message: 'No client associated with user, your are an orphan!'
-                        });
-                    } else if (client) {
-
-                        // Don't store private stuff in the jwt
-                        user.password = undefined;
-                        user.salt = undefined;
-
-                        // Add reseller info to the user
-                        if (client.reseller) {
-                            user.reseller = true;
-                            user.resellerClients = client.resellerClients;
-                        } else {
-                            user.reseller = false;
-                        }
-
-                        var token = jwt.sign(user.toObject(), client.apikey.secret, {
-                            expiresIn: '1d'
-                        });
-
-                        // return the information, including token, as JSON
-                        res.json({
-                            message: 'Authentication successful.',
-                            token: token
-                        });
-                    }
-                });
-            }
-        }
-    }).catch(function() {
-        res.status(404).send({
-            message: 'Error with the database.'
-        });
-    });
 
 };
