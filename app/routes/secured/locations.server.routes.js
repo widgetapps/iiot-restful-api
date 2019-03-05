7ca92547737d886e168c3ed8ee52d643d7aa2c9e
Darryl Patterson <darryl.patterson@terepac.com>
2019-03-05 20:25:59 +0000

    A little bug with the location route.

--- app/routes/secured/locations.server.routes.js
+++ app/routes/secured/locations.server.routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     // Root routing
     var locations = require('../../../app/controllers/locations.server.controller');
 
-    app.route('locations/:locationId')
+    app.route('/locations/:locationId')
         .get(locations.getOne)
         .put(locations.update);
 };
