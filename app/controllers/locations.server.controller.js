3c17e41c8f362a250ecb81b380925b53ac6d3a4c
Darryl Patterson <darryl.patterson@terepac.com>
2019-03-05 19:19:14 +0000

    Added getOne endpoint for Location.

--- app/controllers/locations.server.controller.js
+++ app/controllers/locations.server.controller.js
@@ -8,6 +8,43 @@ var Location = require('@terepac/terepac-models').Location;
 
 exports.getOne = function(req, res) {
 
+    var promise = Location.findById(req.params.locationId).exec();
+
+    promise.then(function(location) {
+        var authorized = false;
+
+        switch (req.user.role) {
+            case 'user':
+                if (req.user.client.toString() === location.client.toString()) {
+                    authorized = true;
+                }
+                break;
+            case 'manufacturer':
+            case 'admin':
+            case 'manager':
+                if (req.user.client.toString() === location.client.toString() || _.includes(req.user.resellerClients, location.client)) {
+                    authorized = true;
+                }
+                break;
+            case 'super':
+                authorized = true;
+                break;
+        }
+
+        if (!authorized) {
+            res.status(401).send({
+                message: 'You are not authorized to access this resource.'
+            });
+            return;
+        }
+
+        res.json(location);
+
+    }).catch(function(error) {
+        res.status(400).send({
+            message: 'Error with the database.'
+        });
+    });
 };
 
 exports.update = function(req, res) {
