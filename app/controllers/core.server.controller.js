d29dc804643df98d3518425ad5c508b307c40456
Darryl Patterson <darryl.patterson@terepac.com>
2019-11-14 14:18:56 -0500

    Working on MySQL migration...

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,12 +1,29 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-    Device = require('@terepac/terepac-models').Device,
+var Device = require('@terepac/terepac-models').Device,
     crypto = require('crypto'),
-    util = require('../lib/util');
+    util = require('../lib/util'),
+    Mqtt = require('@terepac/terepac-models').Mqtt,
+    mysql = require('mysql');
 
 exports.index = function(req, res) {
 	res.json({message: 'Hi there, I\'m a Terepac ONE server. Isn\'t that weird?'});
+};
+
+exports.migrateMqttLogins = function( req, res) {
+    let promise = Mqtt.find({}).exec();
+
+    promise.then(function(logins) {
+        let sql = '';
+        logins.forEach(function(login) {
+            sql = 'INSERT INTO mqtt_user\n';
+        });
+        res.send(sql);
+    }).catch(function(e) {
+        res.json({
+            message: 'ERROR: ' + e.toString()
+        });
+    });
 };
 
 exports.dbCheck = function(req, res) {
