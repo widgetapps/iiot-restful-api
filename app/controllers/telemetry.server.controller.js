a7346cbeb47989122929a0c69aaa8dc3e177ad78
Darryl Patterson <widgetapps@gmail.com>
2020-05-21 12:49:35 -0400

    Working on telemetry summery...

--- app/controllers/telemetry.server.controller.js
+++ app/controllers/telemetry.server.controller.js
@@ -211,6 +211,36 @@ exports.getSummarizedTelemetry = function(req, res) {
         'median': {'$push': '$data.values.average'}
     };
     aggregationStages.sort = {'_id.year': 1, '_id.month': 1, '_id.day': 1, '_id.hour': 1, '_id.minute': 1, '_id.second': 1};
+    aggregationStages.project = {
+        'tag': '$_id.tag',
+        'date': {'$dateFromParts': {'year': '$_id.year', 'month': '$_id.month', 'day': '$_id.day', 'hour': '$_id.hour'}},
+        'data.unit': '$unit',
+        'data.count': '$count',
+        'data.values': '$median',
+        'data.summary.first': '$first',
+        'data.summary.last': '$last',
+        'data.summary.min': '$min',
+        'data.summary.max': '$max',
+        'data.summary.mean': '$mean',
+        'data.summary.median': {
+            '$cond': {
+                'if': {
+                    '$eq': [ {$mod: [ {$size: '$median'}, 2 ]}, 0 ]
+                },
+                'then': {
+                    $avg: [
+                        {$arrayElemAt: [ '$median', {$subtract:[ {$divide: [ {$size:'$median'}, 2 ]}, 1 ]} ]},
+                        {$arrayElemAt: [ '$median', {$divide: [ {$size:'$median'}, 2 ]} ]}
+                    ]
+                },
+                'else': {
+                    $arrayElemAt: [ '$median', {$floor : {$divide: [ {$size: '$median'}, 2 ]}} ]
+                }
+            }
+        },
+        'data.summary.sum': '$sum',
+        '_id': 0
+    };
 
 
     res.set({
@@ -222,7 +252,8 @@ exports.getSummarizedTelemetry = function(req, res) {
     Telemetry.aggregate([
         {'$match': aggregationStages.match},
         {'$group': aggregationStages.group},
-        {'$sort': aggregationStages.sort}
+        {'$sort': aggregationStages.sort},
+        {'$project': aggregationStages.project}
     ]).cursor().exec().pipe(JSONStream.stringify()).pipe(res);
 };
 
